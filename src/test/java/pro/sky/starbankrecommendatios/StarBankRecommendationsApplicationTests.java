c6a76f54bd110fe53990f8258c4dbf699af4f872
Black77 <islepuchin@yandex.ru>
2025-03-24 10:44:53 +0300

    Init project

---
+++ src/test/java/pro/sky/starbankrecommendatios/StarBankRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.starbankrecommendatios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankRecommendationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
